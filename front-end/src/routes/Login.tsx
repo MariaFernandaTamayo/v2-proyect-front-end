a8f677a67ffacd76183d6ce9bf6ca725a82ade4a
Maria Fernanda Tamayo <mariatalo@unisabana.edu.co>
2024-05-05 13:52:02 -0500

    front-1

--- front-end/src/routes/Login.tsx
+++ front-end/src/routes/Login.tsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
+import { useState } from "react"
+import { useAuth } from "../auth/AuthProvider";
 export default function Login(){
+    const[username,setUsername]=useState("");
+    const[password,setPassword]=useState("");
+    const auth=useAuth();
+    if(auth.isAuthenticated){
+        return <Navigate to="/dashboard" />
+    }
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="w-full max-w-sm bg-zinc-700 rounded-md shadow-md p-6">
                 <h1>Login</h1>
                 
-                <input type="text" className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
             placeholder="Username"></input>
                                
                
-                <input type="password" className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
+                <input type="password" value= {password} onChange={(e) => setPassword(e.target.value)} className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
             placeholder="Password"></input>
             <button 
                 type="submit" 
--- front-end/src/routes/Register.tsx
+++ front-end/src/routes/Register.tsx
@@ -1,19 +1,29 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, Navigate } from "react-router-dom"
+import { useAuth } from "../auth/AuthProvider";
 export default function Register(){
+    const[username,setUsername]=useState("");
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    const auth=useAuth();
+    if(auth.isAuthenticated){
+        return <Navigate to="/dashboard" />
+    }
+
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="w-full max-w-sm bg-zinc-700 rounded-md shadow-md p-6">
                 <h1>Register</h1>
                 
-                <input type="text" className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
             placeholder="Username"></input>
 
                 
-                <input type="email"className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
             placeholder="Email"></input>
 
                 
-                <input type="password" className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
+                <input type="password" value= {password} onChange={(e) => setPassword(e.target.value)} className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md border border-purple-500 focus:outline-none focus:border-purple-700 mb-2" 
             placeholder="Password"></input>
             <button 
                 type="submit" 
